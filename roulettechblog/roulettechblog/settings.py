116251e99e7df1b78b3488ae2f920c0f12348871
Sumedh Patkar <sumedh.patkar19@gmail.com>
2024-07-19 12:10:07 -0400

    Fixed production build bug

--- roulettechblog/roulettechblog/settings.py
+++ roulettechblog/roulettechblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(@e&q-nosis+jzg)2qispbzrgegz&f-m0ehp8anui^awoektlu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS=['localhost', '127.0.0.1']
 
